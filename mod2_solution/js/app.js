e21b4f3207161f825db855040bfa04a6dbb567ad
r3tr0sp3c <47220281+soppilif@users.noreply.github.com>
2020-12-29 00:25:37 +0200

    Almost ready

--- mod2_solution/js/app.js
+++ mod2_solution/js/app.js
@@ -1,22 +1,46 @@
+// Inside IIFE
+(function() {
+
 angular.module('myApp', [])
 .controller('toBuyListController', toBuyListController)
-.controller('boughtListController', boughtListController);
+.controller('boughtListController', boughtListController)
+.service('listService', listService);
 
-function toBuyListController() {
+toBuyListController.$inject = ['listService'];
+function toBuyListController(listService) {
     var buyList = this;
-    buyList.list = [{ name: 'Cookies', quantity: 10} ,
-                { name: 'Chips', quantity: 5},
-                { name: 'Soft Drinks', quantity: 3},
-                { name: 'Pastry', quantity: 1},
-                { name: 'Cappuccino', quantity: 3}];
+    buyList.list = listService.buyList;
     
     buyList.showIndex = function(index) {
-        console.log(index);
+        listService.clickAway(index);
     }
     
 
 }
 
-function boughtListController() {
+boughtListController.$inject = ['listService'];
+function boughtListController(listService) {
     var boughtList = this;
+    boughtList.list = listService.boughtList;
 }
+
+function listService() {
+    service = this;
+    service.buyList = [
+        { name: 'Cookies', quantity: 10} ,
+        { name: 'Chips', quantity: 5},
+        { name: 'Soft Drinks', quantity: 3},
+        { name: 'Pastry', quantity: 1},
+        { name: 'Cappuccino', quantity: 3}
+    ];
+
+    service.boughtList = [];
+
+    service.clickAway = function(index) {
+        var item = service.buyList.splice(index,1);
+        service.boughtList.push(item[0]);
+    }
+
+}
+
+})();
